399ebe9e8860bc7420e624b8b9a1dc35d1cc89fc
Jennifer <jennifer@cca.edu>
2019-10-03 07:55:49 -0700

    many updates

---
+++ zine/main.js
@@ -0,0 +1,12 @@
+
+
+let pages = document.querySelectorAll('.page')
+
+console.log(pages)
+
+window.addEventListener('scroll',function(){
+	let index = Math.floor((window.scrollY + window.innerHeight/2) / window.innerHeight)
+
+
+	pages[index].classList.add('visited')
+})
